1ea86ae667fc8e78884e7439433b962146fc3731
carrillo21108 <87512111+carrillo21108@users.noreply.github.com>
2022-03-04 22:50:50 -0600

    Agregacion Base Interprete

---
+++ src/uvg/edu/common/IInterpreter.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package uvg.edu.common;
+
+/**
+ * @author Brian Carrillo
+ *
+ */
+public interface IInterpreter {
+	/***
+	 * Este metodo evalua una expresion en notacion infija y retorna la notacion posfija
+	 * Precondicion:
+	 * @param expresion: es la expresion simple "1 + 2" (String)
+	 * Postcondicion:
+	 * @return String: " 1 2 + "
+	 */
+	String Evaluate(String expresion);
+}
---
+++ src/uvg/edu/common/Interpreter.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package uvg.edu.common;
+
+/**
+ * @author Brian Carrillo
+ *
+ */
+public class Interpreter implements IInterpreter {
+
+	@Override
+	public String Evaluate(String expresion) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
