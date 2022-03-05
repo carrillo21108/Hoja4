c02c36b96eead97c54e774acc7dfd9d7982c8362
lop21666 <91706525+lop21666@users.noreply.github.com>
2022-03-05 00:01:31 -0600

    Implementación Iterpreter, ListIntanceCreator, StackInstanceCreator y renombre StackUsingSingleLinkedList

--- src/uvg/edu/common/StackInstanceCreator.java
+++ src/uvg/edu/common/StackInstanceCreator.java
@@ -2,11 +2,45 @@
  * 
  */
 package uvg.edu.common;
+import uvg.edu.structures.Stack;
+import uvg.edu.structures.StackUsingArrayList;
+import uvg.edu.structures.StackUsingDoubleLinkedList;
+import uvg.edu.structures.StackUsingSingleLinkedList;
+import uvg.edu.structures.StackUsingVector;
 
 /**
  * @author Brian Carrillo
  *
  */
 public class StackInstanceCreator {
+	
+	public ListInstanceCreator listFactory = new ListInstanceCreator();
+	
+	public Stack getInstance(String typeStack) {
+		Stack myStack;
+		
+		switch(typeStack) {
+		case "ArrayList":
+			myStack = new StackUsingArrayList<Integer>();
+			break;
+		
+		case "Vector":
+			myStack = new StackUsingVector<Integer>();
+			break;
+		
+		case "SingleLinkedList":
+			myStack = listFactory.getInstace(typeStack);
+			break;
+			
+		case "DoubleLinkedList":
+			myStack = listFactory.getInstace(typeStack);
+			break;
+			
+		default:
+			myStack = null;
+			break;
+		}
+		return myStack;
+	}
 
 }
