da8f0d75cfd2d2ecf8fecc33a3da0e8b5badf883
wachuuu <88355602+Wachuuu15@users.noreply.github.com>
2022-03-05 16:36:52 -0600

    principal reader

--- src/uvg/edu/main/Principal.java
+++ src/uvg/edu/main/Principal.java
@@ -2,6 +2,9 @@
  * 
  */
 package uvg.edu.main;
+import uvg.edu.io.*;
+import java.util.Scanner;
+
 
 /**
  * @author Brian Carrillo
@@ -13,8 +16,34 @@ public class Principal {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
+	        Scanner sc = new Scanner(System.in);
+	        boolean salida = false;
 
-	}
+	        while(!salida){
 
-}
+	            System.out.println("Elija la opcion que necesite ejecutar");
+	            System.out.println("1. Ingresar la ruta del archivo");
+	            System.out.println("2. Salir");
+	            int opc = sc.nextInt();
+	            sc.nextLine();
+
+	            switch (opc) {
+	                case 1:
+	                String pathway = sc.nextLine();
+	                Reader reader = new Reader(pathway);
+	                Scanner evaluate = new Scanner(reader.leer());
+	 
+	            }
+
+
+
+	        }
+	        sc.close();        
+
+	    }
+
+
+	}		
+
