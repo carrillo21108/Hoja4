dd4eabddba2f4760245b43d188cbb41bf8d49de9
carrillo21108 <87512111+carrillo21108@users.noreply.github.com>
2022-03-05 18:23:39 -0600

    Creacion de Clases de Pruebas

---
+++ test/uvg/edu/test/PosfixCalcTest.java
@@ -0,0 +1,14 @@
+package uvg.edu.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PosfixCalcTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
